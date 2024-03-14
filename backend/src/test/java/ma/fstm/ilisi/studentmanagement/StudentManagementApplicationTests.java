21406879b62d5040035c73e5634295d031460b20
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-14 20:11:34 +0000

    setup backend with spring boot

---
+++ backend/src/test/java/ma/fstm/ilisi/studentmanagement/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fstm.ilisi.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
