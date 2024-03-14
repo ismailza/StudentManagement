9af8ec85a08c263deedd3c10565f48fce1189d19
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-14 20:13:40 +0000

    add Course Not Found Exception

---
+++ backend/src/main/java/ma/fstm/ilisi/studentmanagement/exception/CourseNotFoundException.java
@@ -0,0 +1,9 @@
+package ma.fstm.ilisi.studentmanagement.exception;
+
+public class CourseNotFoundException extends Exception {
+
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+
+}
