2854c74e7afdd6527ea18d49c81221c34a7f7541
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-14 21:14:00 +0000

    add grade exception

---
+++ backend/src/main/java/ma/fstm/ilisi/studentmanagement/exception/GradeNotFoundException.java
@@ -0,0 +1,9 @@
+package ma.fstm.ilisi.studentmanagement.exception;
+
+public class GradeNotFoundException extends Exception {
+
+    public GradeNotFoundException(String message) {
+        super(message);
+    }
+
+}
