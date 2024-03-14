b06c7b31c639bdcb3f37d786d83b266ca946ee76
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-14 20:13:13 +0000

    add Student Not Found Exception

---
+++ backend/src/main/java/ma/fstm/ilisi/studentmanagement/exception/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package ma.fstm.ilisi.studentmanagement.exception;
+
+public class StudentNotFoundException extends Exception {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+}
